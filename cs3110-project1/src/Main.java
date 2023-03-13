9b73e9f480107d5001eba6f09bf1eca2269d2967
dmarcha <dmarcha@cpp.edu>
2023-03-12 23:37:29 -0700

    changed code to simulate OctInteger NFA

--- cs3110-project1/src/Main.java
+++ cs3110-project1/src/Main.java
@@ -7,21 +7,19 @@ public class Main {
     public static void main(String[] args) throws Exception {
         State[] states = {
                 new State(0, false),
-                new State(1, true),
-                new State(2, true),
-                new State(3, false),
+                new State(1, false),
+                new State(2, false),
+                new State(3, true),
                 new State(4, false),
         };
 
         Transition[] transitions = {
-                new Transition(states[0], states[1], new char[] { '1','2','3','4','5','6','7','8','9' }),
-                new Transition(states[0], states[2], new char[] { '0' }),
-                new Transition(states[1], states[1], new char[] { '0','1','2','3','4','5','6','7','8','9' }),
-                new Transition(states[2], states[2], new char[] { '0' }),
-                new Transition(states[1], states[3], new char[] { '-'}),
-                new Transition(states[2], states[4], new char[] { '-'}),
-                new Transition(states[3], states[1], new char[] { '0','1','2','3','4','5','6','7','8','9' }),
-                new Transition(states[4], states[2], new char[] { '0' }),
+                new Transition(states[0], states[1], new char[] { '0' }),
+                new Transition(states[1], states[2], new char[] { 'o', 'O' }),
+                new Transition(states[2], states[3], new char[] { '0','1','2','3','4','5','6','7' }),
+                new Transition(states[3], states[3], new char[] { '0','1','2','3','4','5','6','7' }),
+                new Transition(states[3], states[4], new char[] { '-'}),
+                new Transition(states[4], states[3], new char[] { '0','1','2','3','4','5','6','7' }),
         };
 
         Automaton nfa = new Automaton(transitions, states[0]);
